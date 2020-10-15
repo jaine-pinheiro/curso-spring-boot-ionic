854985bf9f9f8ebb1f0b797b9b643bd7e45b009c
Jaine Pinheiro <jaine.pinheiro@gmail.com>
2020-10-15 14:47:01 -0300

    Ajustes na busca de categoria por id

--- src/main/java/com/jp/cursomc/services/CategoriaService.java
+++ src/main/java/com/jp/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jp.cursomc.domain.Categoria;
 import com.jp.cursomc.repositories.CategoriaRepository;
+import com.jp.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 	}
 	
 }
