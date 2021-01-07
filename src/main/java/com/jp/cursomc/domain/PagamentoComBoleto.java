a355a560246a80cdf81dc46bb37fdf94ffeb79c8
Jaine Pinheiro <jaine.pinheiro@gmail.com>
2021-01-07 13:57:48 -0300

    Adicionando Endpoints de pedido

--- src/main/java/com/jp/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/jp/cursomc/domain/PagamentoComBoleto.java
@@ -3,6 +3,8 @@ package com.jp.cursomc.domain;
 import java.util.Date;
 
 import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jp.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -10,7 +12,10 @@ public class PagamentoComBoleto extends Pagamento {
 	
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
